5d92b5db81cabcb35ad4ac3882740e976dc3d30d
Balamurugan Nagarajan <bala@Balamurugans-MacBook-Air.local>
2023-06-26 16:52:04 +0530

    Initial commit

---
+++ scripts/deployFaucet.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy("0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9");
+
+  await faucet.deployed();
+
+  console.log("Facuet contract deployed: ", faucet.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
